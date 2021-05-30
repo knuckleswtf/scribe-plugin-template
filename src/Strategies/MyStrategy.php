b607dc453e5128366914c7161d42225f6ebc87aa
Shalvah <shalvah@users.noreply.github.com>
2021-05-30 13:19:04 +0100

    Prep for v3

--- src/Strategies/MyStrategy.php
+++ src/Strategies/MyStrategy.php
@@ -2,47 +2,30 @@
 
 namespace Me\MyScribePlugin\Strategies;
 
-use Illuminate\Routing\Route;
 use Knuckles\Scribe\Extracting\ParamHelpers;
-use Knuckles\Scribe\Extracting\RouteDocBlocker;
 use Knuckles\Scribe\Extracting\Strategies\Strategy;
-use ReflectionClass;
-use ReflectionFunctionAbstract;
+use Knuckles\Camel\Extraction\ExtractedEndpointData;
 
 class MyStrategy extends Strategy
 {
     /**
-     * The stage the strategy belongs to.
-     * One of "metadata", "urlParameters", "queryParameters", "bodyParameters", "headers", "responses", "responseFields".
-     */
-    public $stage = 'queryParameters';
-
-    /**
-     * Trait containing some helper methods for dealing with "parameters".
-     * Useful if your strategy extracts information about parameters.
+     * Trait containing some helper methods for dealing with "parameters",
+     * such as generating examples and casting values to types.
+     * Useful if your strategy extracts information about parameters or generates examples.
      */
     use ParamHelpers;
 
     /**
-     * @link https://scribe.readthedocs.io/en/latest/plugins.html
-     *
-     * @param Route $route The route which we are currently extracting queryParameters for.
-     * @param ReflectionClass $controller The class handling the current route.
-     * @param ReflectionFunctionAbstract $method The method/closure handling the current route.
+     * @link https://scribe.knuckles.wtf/laravel/advanced/plugins
+     * @param ExtractedEndpointData $endpointData The endpoint we are currently processing.
+     *   Contains details about httpMethods, controller, method, route, url, etc, as well as already extracted data.
      * @param array $routeRules Array of rules for the ruleset which this route belongs to.
-     * @param array $alreadyExtractedData  Data already extracted from previous stages and earlier strategies in this stage
      *
      * See the documentation linked above for more details about writing custom strategies.
      *
      * @return array|null
      */
-    public function __invoke(
-        Route $route,
-        ReflectionClass $controller,
-        ReflectionFunctionAbstract $method,
-        array $routeRules,
-        array $alreadyExtractedData = []
-    )
+    public function __invoke(ExtractedEndpointData $endpointData, array $routeRules): ?array
     {
 
         return $this->getData($route, $controller, $method);
